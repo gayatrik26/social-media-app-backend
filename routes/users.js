31c01e5cd79d60878a7da6aa149d36dc196ae92b
Gayatri <gayatrik22803@gmail.com>
2024-09-04 18:09:25 +0530

    initialized

---
+++ routes/users.js
@@ -0,0 +1,18 @@
+import express from "express";
+import {
+    getUser,
+    getUserFriends,
+    addRemoveFriends,
+} from "../controllers/users.js";
+import { verifyToken } from "../middleware/auth.js";
+
+const router = express.Router();
+
+// read
+router.get("/:id" , verifyToken, getUser);
+router.get("/:id/friends", verifyToken, getUserFriends);
+
+//update
+router.patch("/:id/:friendID", verifyToken, addRemoveFriends);
+
+export default router;
